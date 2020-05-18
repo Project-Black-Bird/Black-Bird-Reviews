33810a1820247b255cf80c95acceba82983373b5
Jeremy Laningham <JeremyLaningham@gmail.com>
2020-05-18 11:39:20 -0500

    pulling navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import "./Navbar.scss";
 
+
 class Navbar extends Component {
   constructor(props) {
     super(props);
