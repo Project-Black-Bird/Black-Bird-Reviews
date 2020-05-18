6ee2a947ea8d95aad673f8ba70f807cc07fdd680
pembalama <pemba.t.lama@gmail.com>
2020-05-18 10:59:54 -0500

    added login modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AuthModal from './Components/Modal/AuthModal';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World!!!
-    </div>
-  );
+	return (
+		<div className="App">
+			Hello World!!!
+			<AuthModal />
+		</div>
+	);
 }
 
 export default App;
