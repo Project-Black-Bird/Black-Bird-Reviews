772386d43f00dd80184440d68b5a2b9295bc3044
= <=>
2020-05-19 16:16:20 -0500

    initial mock of posts component

---
+++ src/Components/Posts/Posts.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import Post from "../Post";
+import posts from "./POSTS_MOCK.json";
+class Posts extends React.Component {
+  state = {
+    posts: [],
+  };
+  constructor(props) {
+    super(props);
+  }
+  getAllPosts() {
+    this.setState({ posts: posts });
+  }
+  renderPosts() {
+    return this.state.posts.map((post) => {
+      <Post
+        title="Example Post"
+        review="Blank review"
+        likes={0}
+        {...this.props.user}
+      />;
+    });
+  }
+  componentDidMount() {
+    this.getAllPosts();
+  }
+  render() {
+    return <div className="Posts-container">{this.state.posts}</div>;
+  }
+}
