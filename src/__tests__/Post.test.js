fb34e9ccb085b8c72ad850fa0217bfdf0ba5830b
= <=>
2020-05-19 11:09:29 -0500

    ready to merge charlie's code

---
+++ src/__tests__/Post.test.js
@@ -0,0 +1,7 @@
+///<reference types='jest' />
+import React from "react";
+import Post from "../Components/Post.jsx";
+import renderer from "react-test-renderer";
+describe("Post Component", () => {
+  test("should mount", () => {});
+});
