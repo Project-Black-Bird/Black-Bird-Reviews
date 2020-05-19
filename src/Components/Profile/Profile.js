41bc194ab0be42079026668b11e4658e45499f36
Jeremy Laningham <JeremyLaningham@gmail.com>
2020-05-19 11:09:05 -0500

    navbar

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -2,9 +2,48 @@ import React, { Component } from 'react';
 import "./Profile.scss";
 import axios from "axios";
 import { connect } from "react-redux";
+import { getUser } from "../../redux/reducer";
 import { logoutUser } from "../../redux/reducer";
+import { withRouter } from "react-router-dom";
 
 class Profile extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user: '',
+            email: '',
+            editView: false
+        }
+    }
+
+    componentDidMount(){
+        if(!this.props.user.email){
+            this.props.history.push('/')
+        }
+    }
+
+    handleInput = (val) => {
+        this.setState({email: val})
+    }
+
+    handleEditView = () => {
+        this.setState({editView: !this.state.editView})
+    }
+
+    handleLogout = () => {
+        axios.get('auth/logout')
+        .then(() => {
+            this.props.logoutUser();
+            this.props.history.push('/')
+        })
+        .catch(err => console.log(err))
+    }
+
+    updateUseremail = () => {
+        const {email} = this.state;
+        axios.put(`/api/`)
+    }
+
     render() {
         return (
             <div>
