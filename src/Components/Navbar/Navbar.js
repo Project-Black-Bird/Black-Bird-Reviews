dc63539d4d4362e0e1964331b8e1e3363c47ef47
Jeremy Laningham <JeremyLaningham@gmail.com>
2020-05-22 15:59:26 -0500

    submit fields

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -47,7 +47,7 @@ class Navbar extends Component {
               className="nav-links"
               onClick={() => this.handleToggle}
             >
-              Log In
+              {/* Log In */}
               <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42"><title>ic_person_outline_48px</title><g fill="#ffffff"><path d="M21.06 10.35a3.69 3.69 0 1 1 0 7.38 3.69 3.69 0 0 1 0-7.38m0 15.8c5.22 0 10.71 2.55 10.71 3.68v1.94h-21.42v-1.93c0-1.13 5.49-3.69 10.71-3.69m0-19.13c-3.87 0-7.02 3.15-7.02 7.02s3.15 7.02 7.02 7.02 7.02-3.15 7.02-7.02-3.15-7.02-7.02-7.02z m0 15.8c-4.68 0-14.04 2.34-14.04 7.02v5.26h28.08v-5.26c0-4.68-9.36-7.02-14.04-7.02z"></path></g></svg>
             </Link>
           </h1>
