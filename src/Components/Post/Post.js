76c7164fef64655b8a38be78351f24def8cbb56d
= <=>
2020-05-22 10:06:58 -0500

    post now toggles comments

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -5,14 +5,36 @@ import CommentForm from "../Comment/CommentForm";
 import EditPost from "../Post/EditPost/EditPost";
 import "./Post.scss";
 import Comments from "../Comments/Comments";
+import Comment from "../Comment/Comment";
+import CommentsMock from "../Comments/COMMENTS_MOCK.json";
 class Post extends React.Component {
   state = {
     edit: false,
+    showComments: false,
+    commentsLimit: 3,
+    comments: [],
+    renderedComments: [],
   };
   constructor(props) {
     super(props);
     this.enterEditMode = this.enterEditMode.bind(this);
     this.doneEditing = this.doneEditing.bind(this);
+    this.toggleComments = this.toggleComments.bind(this);
+    this.getMoreComments = this.getMoreComments.bind(this);
+  }
+  toggleComments() {
+    if (this.state.showComments === false) {
+      // get the latest comments
+      this.setState({ ...this.state, comments: CommentsMock }, () => {
+        this.setState({
+          ...this.state,
+          renderedComments: this.renderComments(),
+          showComments: true,
+        });
+      });
+    } else {
+      this.setState({ ...this.state, showComments: !this.state.showComments });
+    }
   }
   enterEditMode() {
     this.setState({ ...this.state, edit: !this.state.edit });
@@ -21,6 +43,29 @@ class Post extends React.Component {
     this.props.updatePost(this.props.post_id, title, image, review);
     this.setState({ ...this.state, edit: false });
   }
+  renderComments(arr) {
+    let renderedComments = [];
+    for (
+      let commentIndex = 0;
+      commentIndex <
+      (this.state.commentsLimit < this.state.comments.length
+        ? this.state.commentsLimit
+        : this.state.comments.length);
+      commentIndex++
+    ) {
+      let comment = this.state.comments[commentIndex];
+      renderedComments.push(
+        <Comment
+          comment_id={comment.comment_id}
+          text={comment.text}
+          username={comment.username}
+        />
+      );
+      this.setState({ ...this.state, renderedComments });
+    }
+    return renderedComments;
+  }
+  getMoreComments() {}
   render() {
     return (
       <div className="Post-container">
@@ -52,7 +97,13 @@ class Post extends React.Component {
                   <button className="delete">Delete</button>
                 </span>
               ) : null}
-              <span className="like-share">
+              <span className="like-share-comments">
+                <button
+                  type="button"
+                  className="comment"
+                  onClick={this.toggleComments}>
+                  {(this.props.comments || []).length} Comments
+                </button>
                 <button type="button" className="like">
                   {`Like ${this.props.likes || 0}`}
                 </button>
@@ -66,7 +117,13 @@ class Post extends React.Component {
         {this.props.user.user_id && this.props.user.email ? (
           <CommentForm />
         ) : null}
-        <Comments />
+        {this.state.showComments ? (
+          <div className="Comments-container">
+            {this.state.renderedComments}
+            <button>load more comments</button>
+            <button onClick={this.toggleComments}>Hide Comments</button>
+          </div>
+        ) : null}
       </div>
     );
   }
