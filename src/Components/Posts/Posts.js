4220f1345f2a24dc7ef1a1c9be923a828a90160e
pembalama <pemba.t.lama@gmail.com>
2020-05-25 11:08:07 -0500

    get new code

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -71,11 +71,26 @@ class Posts extends React.Component {
 		return (
 			<div className="Posts-container">
 				<CreatePost />
-				<input
-					className="search-input"
-					placeholder="Search Title"
-					onChange={this.onchange}
-				/>
+				<div>
+					<form>
+						<label className="search-icon">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path d="M15.59,13.91l2.78,2.69a1.25,1.25,0,1,1-1.74,1.8l-2.82-2.73a8,8,0,1,1,1.78-1.76ZM14.64,9.2A5.45,5.45,0,1,0,9.2,14.64,5.45,5.45,0,0,0,14.64,9.2Z" />
+							</svg>
+						</label>
+						<input
+							className="search-input"
+							placeholder="Search Title"
+							onChange={this.onchange}
+						/>
+					</form>
+				</div>
+
 				<div className="posts">{this.renderPosts()}</div>
 			</div>
 		);
