36e95a784567136bf4ea7703769c6563b085d236
= <=>
2020-05-28 21:25:10 -0500

    updated server to parse DATABASE_URL for massive

--- server/index.js
+++ server/index.js
@@ -6,17 +6,20 @@ const routes = require("./routes");
 const path = require("path");
 const app = express();
 
-const {
-  DATABASE_URL,
-  DATABASE_USERNAME,
-  DATABASE_PASSWORD,
-  DATABASE_HOST,
-  DATABASE_PORT,
-  DATABASE_NAME,
+const { DATABASE_URL, SESSION_SECRET, SSL_MODE } = process.env;
 
-  SESSION_SECRET,
-  SSL_MODE,
-} = process.env;
+if (DATABASE_URL == null) {
+  console.error("DATABASE CREDS WERE NULL OR NOT DEFINED! APP WILL NOT START");
+}
+let DbCreds = /^postgres:\/\/([A-Za-z0-9]+)\:([0-9A-Za-z]+)@([A-Za-z0-9\.\-]+):(\d+)\/([a-zA-Z0-9]+)/.exec(
+  DATABASE_URL
+);
+// console.log(DbCreds);
+let DATABASE_USERNAME = DbCreds[1];
+let DATABASE_PASSWORD = DbCreds[2];
+let DATABASE_HOST = DbCreds[3];
+let DATABASE_PORT = DbCreds[4];
+let DATABASE_NAME = DbCreds[5];
 
 let { SERVER_HOST, SERVER_PORT, HOST, PORT } = process.env;
 
